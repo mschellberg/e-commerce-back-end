a82fedefd992955aa9536a00b28b9385de61ce5f
Melanie <mschellberg27@icloud.com>
2021-03-02 11:01:02 -0500

    setting up project

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+const mysql = require('mysql');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
